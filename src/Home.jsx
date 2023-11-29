01efafe01f7492d7f23ea56202623e95c3753561
Ajaybanu <ajaybanu7@gmail.com>
2023-11-29 22:56:19 +0530

    fetch home page data

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <input type="button" value="" />
+        <button>Click</button>
+    </div>
+  )
+}
+
+export default Home
