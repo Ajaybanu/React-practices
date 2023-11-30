22ce0ad42650a10dc800c37724b94498c6a3814b
Ajaybanu <ajaybanu7@gmail.com>
2023-11-30 23:03:41 +0530

    fhfgh

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home() {
+  const [input,setInput]=useState(" ")
   return (
     <div>
         <input type="button" value="" />
-        <button>Click</button>
+        <button onClick={setInput(e)input=>e.target.value}>Click</button>
+
+        <h1>{input}</h1>
     </div>
   )
 }
