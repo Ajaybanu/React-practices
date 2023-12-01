5f2503e262f61630c05c7d9ca96d3c20920552d8
Ajaybanu <ajaybanu7@gmail.com>
2023-12-01 22:21:44 +0530

    dsfkj

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,14 @@ import './App.css'
 
 function App() {
 
-
- const [state,setState]=useState(false)
- const [count,setCount]=useState(0)
- const [color,setColor]=useState("")
-
- useEffect(()=>{
-  console.log("mounting",count)
-  
-    return()=>{
-      console.log("unmounting",count)
-    }
-
- },[state,count])
+const game1 = "hiiii"
 
 
   return (
 
     <div className='App'>
-
-      <button onClick={()=>setState(!state)}>show and hide</button>
-      {state ? <h1>hello Ajay</h1> : null }
-         
-         <button onClick={()=>setCount(count+1)}> increment</button>
-         <button onClick={()=>setCount(count-1)}>decrement</button>
-         <h1>count {count} </h1>\
-        
-
-
-        <button onClick={()=>setColor(state)}>color button</button>
-        
-        <div className='noo' style={{ color:"{ color ? black:blue"}}>
-
-        </div>
+      <button>click</button>
+      <h1>{game1}</h1>
 
       
     </div>
